bb0e6f927bbc3b8c028c2c5af0ff761cdb93f3e5
hej090224 <fc49854985@gmail.com>
2025-09-15 21:01:06 +0900

    feat: add BoardPersistenceAdapter

--- src/main/java/com/example/clean/adapter/out/peristence/BoardPersitenceAdapter.java
+++ src/main/java/com/example/clean/adapter/out/peristence/BoardPersitenceAdapter.java
@@ -1,4 +1,36 @@
 package com.example.clean.adapter.out.peristence;
 
-public class BoardPersitenceAdapter {
+import com.example.clean.domain.model.Board;
+import com.example.clean.domain.port.out.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardPersitenceAdapter implements BoardRepository {
+
+    private final JpaBoardRepository jpaBoardRepository;
+
+    @Override
+    public Board save(Board board) {
+        return jpaBoardRepository.save(board);
+    }
+
+    @Override
+    public List<Board> findAll() {
+        return jpaBoardRepository.findAll();
+    }
+
+    @Override
+    public Optional<Board> findById(Long id) {
+        return jpaBoardRepository.findById(id);
+    }
+
+    @Override
+    public void deleteboard(Long id) {
+        jpaBoardRepository.deleteById(id);
+    }
 }
