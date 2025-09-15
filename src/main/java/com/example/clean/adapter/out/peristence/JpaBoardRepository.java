f17c943fc7ef15047ba7c2685a9c01d939c1bcf1
hej090224 <fc49854985@gmail.com>
2025-09-15 20:56:13 +0900

    feat: add JpaBoardRepository repository

--- src/main/java/com/example/clean/adapter/out/peristence/JpaBoardRepository.java
+++ src/main/java/com/example/clean/adapter/out/peristence/JpaBoardRepository.java
@@ -1,4 +1,7 @@
 package com.example.clean.adapter.out.peristence;
 
-public class JpaBoardRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.clean.domain.model.Board;
+
+public interface JpaBoardRepository extends JpaRepository<Board, Long> {
 }
