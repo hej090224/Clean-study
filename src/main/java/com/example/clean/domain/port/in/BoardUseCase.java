66c330f8ce8860646d1ec9f049d84a0129d87429
hej090224 <fc49854985@gmail.com>
2025-09-15 20:43:16 +0900

    feat: add BoardUseCase interface

--- src/main/java/com/example/clean/domain/port/in/BoardUseCase.java
+++ src/main/java/com/example/clean/domain/port/in/BoardUseCase.java
@@ -1,4 +1,13 @@
 package com.example.clean.domain.port.in;
 
-public class BoardUseCase {
+import com.example.clean.domain.model.Board;
+
+import java.util.List;
+
+public interface BoardUseCase {
+    Board createBoard(String title, String content);
+    List<Board> getBoards();
+    Board getBoard(Long id);
+    Board updateBoard(Long id, String title, String content);
+    void deleteboard(Long id);
 }
