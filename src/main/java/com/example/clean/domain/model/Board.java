d8aa88d397e577bdb2cfb352b4c18a59a9f6d713
hej090224 <fc49854985@gmail.com>
2025-09-15 20:39:27 +0900

    feat: add Board entity

--- src/main/java/com/example/clean/domain/model/Board.java
+++ src/main/java/com/example/clean/domain/model/Board.java
@@ -1,4 +1,27 @@
 package com.example.clean.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
